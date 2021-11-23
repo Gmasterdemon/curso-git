7b9c5ae8b09d8f447a21b7cb4f83f649b167c099
Gmasterdemon <gleize.martin@gmail.com>
2021-11-23 13:48:06 -0300

    testando

---
+++ GitEGithub/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitEGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
+        }
+    }
+}
