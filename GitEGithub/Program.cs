4f9d91edb52b126ee87c0c2a790ee2899f450394
Gmasterdemon <gleize.martin@gmail.com>
2021-11-23 14:07:08 -0300

    Adicionada Boa tarde

--- GitEGithub/Program.cs
+++ GitEGithub/Program.cs
@@ -8,6 +8,8 @@ namespace GitEGithub
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Boa tarde");
+
         }
     }
 }
